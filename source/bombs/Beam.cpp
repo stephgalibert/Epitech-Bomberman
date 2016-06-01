0f398e755fd56cab6f677ee7895ff3483fea3879
galibe_s <stephane.galibert@epitech.eu>
2016-06-01 15:47:29 +0200

    bomb beam + color

---
+++ source/bombs/Beam.cpp
@@ -0,0 +1,243 @@
+//
+// Beam.cpp for indie in /home/galibe_s/rendu/bomberman/source/bombs
+//
+// Made by stephane galibert
+// Login   <galibe_s@epitech.net>
+//
+// Started on  Tue May 31 20:50:44 2016 stephane galibert
+// Last update Wed Jun  1 15:35:04 2016 stephane galibert
+//
+
+#include "Beam.hpp"
+
+bbman::ABeam::ABeam(Irrlicht &irr, size_t range)
+  : _irr(irr)
+{
+  this->_running = false;
+  this->_range = range;
+  //this->_vl = NULL;
+  this->_repeat = 0;
+  //this->_glow = NULL;
+}
+
+bbman::ABeam::~ABeam(void)
+{
+  /*if (this->_vl) {
+    this->_vl->remove();
+    }*/
+  /*if (this->_glow) {
+    this->_glow->drop();
+    }*/
+  for (auto it : this->_vls) {
+    it->remove();
+  }
+}
+
+void bbman::ABeam::setRepeat(size_t n)
+{
+  this->_repeat = n;
+}
+
+void bbman::ABeam::setPosition(irr::core::vector3df const& pos)
+{
+  this->_pos = pos;
+}
+
+
+bbman::NorthernBeam::NorthernBeam(Irrlicht &irr, size_t range)
+  : ABeam(irr, range)
+{
+
+}
+
+bbman::NorthernBeam::~NorthernBeam(void)
+{
+
+}
+
+void bbman::NorthernBeam::init(Irrlicht &irr, std::string const& color)
+{
+  /*this->_vl =
+    irr.getSmgr()->addVolumeLightSceneNode(0, -1, 32, 32,
+					   irr::video::SColor(0, 255, 255, 255),
+					   irr::video::SColor(0, 0, 0, 0));
+  if (this->_vl) {
+  this->_vl->setScale(irr::core::vector3df(10, 10, 10));*/
+      //irr::core::array<irr::video::ITexture *> textures;
+    for (irr::s32 g = 7 ; g > 0 ; --g) {
+      irr::core::stringc tmp;
+      tmp = "./asset/Beam/vbeam";
+      tmp += color.c_str();
+      tmp += g;
+      tmp += ".png";
+      this->_textures.push_back(irr.getTexture(tmp.c_str()));
+    }
+    //}
+}
+
+void bbman::NorthernBeam::play(void)
+{
+  irr::scene::ISceneNodeAnimator *glow = NULL;
+  irr::scene::IVolumeLightSceneNode *vl = NULL;
+
+  if (!this->_running /*&& this->_vl*/) {
+    this->_running = true;
+    for (int i = 0 ; i < this->_repeat ; ++i)
+      {
+	vl = this->_irr.getSmgr()->addVolumeLightSceneNode(0, -1, 32, 32, irr::video::SColor(0, 255, 255, 255), irr::video::SColor(0, 0, 0, 0));
+	glow = this->_irr.getSmgr()->createTextureAnimator(this->_textures, 70);
+	irr::core::vector3df pos = this->_pos;
+	pos.Z += (10 * (i + 1));
+	vl->setPosition(pos);
+	vl->setScale(irr::core::vector3df(10, 10, 10));
+	vl->addAnimator(glow);
+	glow->drop();
+	this->_vls.push_back(vl);
+	//this->_glows.push_back(glow);
+      }
+  }
+}
+
+/*void bbman::NorthernBeam::setScale(irr::core::vector3df const& scale)
+{
+  irr::core::vector3df fscale = scale;
+  if (this->_vl) {
+    this->_vl->setScale(fscale);
+  }
+  }*/
+
+
+bbman::SouthernBeam::SouthernBeam(Irrlicht &irr, size_t range)
+  : ABeam(irr, range)
+{
+
+}
+
+bbman::SouthernBeam::~SouthernBeam(void)
+{
+
+}
+
+void bbman::SouthernBeam::init(Irrlicht &irr, std::string const& color)
+{
+  for (irr::s32 g = 7 ; g > 0 ; --g) {
+    irr::core::stringc tmp;
+    tmp = "./asset/Beam/vbeam";
+    tmp += color.c_str();
+    tmp += g;
+    tmp += ".png";
+    this->_textures.push_back(irr.getTexture(tmp.c_str()));
+  }
+}
+
+void bbman::SouthernBeam::play(void)
+{
+  irr::scene::ISceneNodeAnimator *glow = NULL;
+  irr::scene::IVolumeLightSceneNode *vl = NULL;
+
+  if (!this->_running) {
+    this->_running = true;
+    for (size_t i = 0 ; i < this->_repeat ; ++i)
+      {
+	vl = this->_irr.getSmgr()->addVolumeLightSceneNode(0, -1, 32, 32, irr::video::SColor(0, 255, 255, 255), irr::video::SColor(0, 0, 0, 0));
+	glow = this->_irr.getSmgr()->createTextureAnimator(this->_textures, 70);
+	irr::core::vector3df pos = this->_pos;
+	pos.Z -= (10 * (i + 1));
+	vl->setPosition(pos);
+	vl->setScale(irr::core::vector3df(10, 10, 10));
+	vl->addAnimator(glow);
+	glow->drop();
+	this->_vls.push_back(vl);
+      }
+  }
+}
+
+bbman::EasternBeam::EasternBeam(Irrlicht &irr, size_t range)
+  : ABeam(irr, range)
+{
+
+}
+
+bbman::EasternBeam::~EasternBeam(void)
+{
+
+}
+
+void bbman::EasternBeam::init(Irrlicht &irr, std::string const& color)
+{
+  for (irr::s32 g = 7 ; g > 0 ; --g) {
+    irr::core::stringc tmp;
+    tmp = "./asset/Beam/hbeam";
+    tmp += color.c_str();
+    tmp += g;
+    tmp += ".png";
+    this->_textures.push_back(irr.getTexture(tmp.c_str()));
+  }
+}
+
+void bbman::EasternBeam::play(void)
+{
+  irr::scene::ISceneNodeAnimator *glow = NULL;
+  irr::scene::IVolumeLightSceneNode *vl = NULL;
+
+  if (!this->_running) {
+    this->_running = true;
+    for (size_t i = 0 ; i < this->_repeat ; ++i)
+      {
+	vl = this->_irr.getSmgr()->addVolumeLightSceneNode(0, -1, 32, 32, irr::video::SColor(0, 255, 255, 255), irr::video::SColor(0, 0, 0, 0));
+	glow = this->_irr.getSmgr()->createTextureAnimator(this->_textures, 70);
+	irr::core::vector3df pos = this->_pos;
+	pos.X += (10 * (i + 1));
+	vl->setPosition(pos);
+	vl->setScale(irr::core::vector3df(10, 10, 10));
+	vl->addAnimator(glow);
+	glow->drop();
+	this->_vls.push_back(vl);
+      }
+  }
+}
+
+bbman::WesternBeam::WesternBeam(Irrlicht &irr, size_t range)
+  : ABeam(irr, range)
+{
+
+}
+
+bbman::WesternBeam::~WesternBeam(void)
+{
+
+}
+
+void bbman::WesternBeam::init(Irrlicht &irr, std::string const& color)
+{
+  for (irr::s32 g = 7 ; g > 0 ; --g) {
+    irr::core::stringc tmp;
+    tmp = "./asset/Beam/hbeam";
+    tmp += color.c_str();
+    tmp += g;
+    tmp += ".png";
+    this->_textures.push_back(irr.getTexture(tmp.c_str()));
+  }
+}
+
+void bbman::WesternBeam::play(void)
+{
+  irr::scene::ISceneNodeAnimator *glow = NULL;
+  irr::scene::IVolumeLightSceneNode *vl = NULL;
+
+  if (!this->_running) {
+    this->_running = true;
+    for (size_t i = 0 ; i < this->_repeat ; ++i)
+      {
+	vl = this->_irr.getSmgr()->addVolumeLightSceneNode(0, -1, 32, 32, irr::video::SColor(0, 255, 255, 255), irr::video::SColor(0, 0, 0, 0));
+	glow = this->_irr.getSmgr()->createTextureAnimator(this->_textures, 70);
+	irr::core::vector3df pos = this->_pos;
+	pos.X -= (10 * (i + 1));
+	vl->setPosition(pos);
+	vl->setScale(irr::core::vector3df(10, 10, 10));
+	vl->addAnimator(glow);
+	glow->drop();
+	this->_vls.push_back(vl);
+      }
+  }
+}
---
+++ source/bombs/Beam.hpp
@@ -0,0 +1,80 @@
+//
+// Beam.hpp for indie in /home/galibe_s/rendu/bomberman/source/bombs
+//
+// Made by stephane galibert
+// Login   <galibe_s@epitech.net>
+//
+// Started on  Tue May 31 20:43:06 2016 stephane galibert
+// Last update Wed Jun  1 08:21:42 2016 stephane galibert
+//
+
+#ifndef _BEAM_HPP_
+# define _BEAM_HPP_
+
+# include <irrlicht.h>
+
+# include "Irrlicht.hpp"
+
+namespace bbman
+{
+  class ABeam
+  {
+  public:
+    ABeam(Irrlicht &irr, size_t range);
+    virtual ~ABeam(void);
+    virtual void init(Irrlicht &irr, std::string const& color) = 0;
+    //virtual void setScale(irr::core::vector3df const& scale) = 0;
+    virtual void play(void) = 0;
+    void setRepeat(size_t n);
+    void setPosition(irr::core::vector3df const& pos);
+  protected:
+    Irrlicht &_irr;
+    bool _running;
+    size_t _range;
+    //irr::scene::IVolumeLightSceneNode *_vl;
+    std::vector<irr::scene::IVolumeLightSceneNode *> _vls;
+    //std::vector<irr::scene::ISceneNodeAnimator *> _glows;
+    irr::core::vector3df _pos;
+    size_t _repeat;
+    irr::core::array<irr::video::ITexture *> _textures;
+    //irr::scene::ISceneNodeAnimator* _glow;
+  };
+
+  class NorthernBeam : public ABeam
+  {
+  public:
+    NorthernBeam(Irrlicht &irr, size_t range);
+    virtual ~NorthernBeam(void);
+    virtual void init(Irrlicht &irr, std::string const& color);
+    virtual void play(void);
+  };
+
+  class SouthernBeam : public ABeam
+  {
+  public:
+    SouthernBeam(Irrlicht &irr, size_t range);
+    virtual ~SouthernBeam(void);
+    virtual void init(Irrlicht &irr, std::string const& color);
+    virtual void play(void);
+  };
+
+  class EasternBeam : public ABeam
+  {
+  public:
+    EasternBeam(Irrlicht &irr, size_t range);
+    virtual ~EasternBeam(void);
+    virtual void init(Irrlicht &irr, std::string const& color);
+    virtual void play(void);
+  };
+
+  class WesternBeam : public ABeam
+  {
+  public:
+    WesternBeam(Irrlicht &irr, size_t range);
+    virtual ~WesternBeam(void);
+    virtual void init(Irrlicht &irr, std::string const& color);
+    virtual void play(void);
+  };
+}
+
+#endif /* !_BEAM_HPP_ */
