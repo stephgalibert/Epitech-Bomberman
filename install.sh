8650071a6a21ba2e0606e4d8e38794de37bdf39f
seb-saletes <seb.saletes@gmail.com>
2016-06-10 08:44:32 +0200

    update install path

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ if [ ! -d "$BUILD" ]; then
   mkdir build
   # Control will enter here if $DIRECTORY doesn't exist.
 fi
-cd build && cmake ..
+cd build && cmake -DCMAKE_INSTALL_PREFIX:PATH=../bin ..
 echo "##### Start compilation... #####"
 make
-echo "##### Installing the project in bin folder... #####"
-make install && cd ..
+echo "##### Installing the project in ./bin folder... #####"
+make install
 echo "##### Installation successful ! #####"
